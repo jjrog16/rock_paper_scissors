eabeec83819b4e9c900553f263713f7aa022ce9a
Jamaal <jjrog16@gmail.com>
2021-05-17 15:02:31 -0400

    Completed game

---
+++ game.js
@@ -0,0 +1,68 @@
+
+const playerSelection = "rock"
+
+function computerPlay(){
+    let move = ["rock", "paper", "scissors"]
+    let choice = Math.floor(Math.random() * 3)
+    return move[choice]
+}
+
+function playRound(playerSelection, computerSelection){
+    switch(playerSelection){
+        case "rock":
+            if(computerSelection === "paper"){
+                return "c"
+            } else if(computerSelection === "scissors"){
+                return "p"
+            }
+            break
+        case "paper":
+            if(computerSelection === "rock"){
+                return "p"
+            } else if(computerSelection === "scissors"){
+                return "c"
+            }
+            break
+        case "scissors":
+            if(computerSelection === "paper"){
+                return "p"
+            } else if (computerSelection === "rock"){
+                return "c"
+            }
+            break
+    }
+}
+
+function game(){
+    let playerWinCounter = 0
+    let computerWinCounter = 0
+    let roundCounter = 0
+
+    while(roundCounter < 5){
+        let computerChoice = computerPlay()
+        let dialogResults = `Player chose ${playerSelection} and Computer chose ${computerChoice}. `
+        let round = playRound(playerSelection,computerChoice)
+
+        if (round == "p"){
+            playerWinCounter++
+            console.log(dialogResults + "Player Wins!")
+            roundCounter++
+        } else if (round == "c"){
+            computerWinCounter++
+            console.log(dialogResults + "Computer Wins!")
+            roundCounter++
+        } else {
+            console.log(dialogResults + "It's a tie!")
+        }
+    }
+    
+    let winDialog = `Player won ${playerWinCounter} times and Computer won ${computerWinCounter} times. `
+
+    if(playerWinCounter > computerWinCounter){
+        console.log(winDialog + "Player wins it all!")
+    } else if(computerWinCounter > playerWinCounter){
+        console.log(winDialog + "Computer wins it all!")
+    }
+}
+
+game()
